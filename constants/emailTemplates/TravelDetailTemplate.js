90bc0abeab5ad3f34d03f2d694ceb5c3d6e224aa
Saroj12Dangol <saroj9823dangol@gmail.com>
2024-02-12 19:27:31 +0545

    travel detail env

--- constants/emailTemplates/TravelDetailTemplate.js
+++ constants/emailTemplates/TravelDetailTemplate.js
@@ -60,7 +60,7 @@ const TravelDetailTemplate = (
        
         <p>Dear ${guestName},</p>
         <p>We hope this message finds you well. ${brideName} and ${groomName} are thrilled to announce their upcoming wedding, and we would be honoured to have you share in the joy of our special day.</p>
-        <p>If you haven't already, kindly send travel detail through our website at <a href="${process.env.INVITATION_URL}/${eventId}">www.indha.com.au</a> Your prompt response would be greatly appreciated. </p>
+        <p>If you haven't already, kindly send travel detail through our website at <a href="${process.env.TRAVEL_DETAIL_URL}/${eventId}">www.indha.com.au</a> Your prompt response would be greatly appreciated. </p>
        
         <br/>
         <p>To ensure your convenience, we would also appreciate it if you could provide your travel details on our website. This will assist us in arranging for your airport transportation, making your journey to and from the celebration as seamless as possible.</p> 
